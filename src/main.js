b4b1aa7ae81a7c1a51cd7ddeea601bb7d14d7c16
xuhaofeng <xuhaofeng@163.com>
2020-01-06 15:47:58 +0800

    项目准备-normalize样式安装

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './utils/register-vant' // 全局引入 封装注册的Vant 组件模块
+import 'normalize.css' // 全局引入normalize.css
 
 Vue.config.productionTip = false
 
