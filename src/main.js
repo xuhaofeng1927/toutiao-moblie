36cc02d4549fd8a94244531b49c8dfd43ac4a34f
xuhaofeng <xuhaofeng@163.com>
2020-01-06 20:55:28 +0800

    项目准备-配置 REM 适配

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import './utils/register-vant' // 全局引入 封装注册的Vant 组件模块
 import 'normalize.css' // 全局引入normalize.css
-
+import 'amfe-flexible' // 全局引入 动态设置 REM 基准值
 Vue.config.productionTip = false
 
 new Vue({
