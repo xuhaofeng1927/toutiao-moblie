2d66988843528c6c0180ce0785b18548a491c834
xuhaofeng <xuhaofeng@163.com>
2020-01-06 16:54:40 +0800

    项目准备-预处理器文件结构

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <!-- 根路由出口 -->
     <router-view></router-view>
+    <div>测试样式设置</div>
+
+    <van-icon name="like-o" />
     <van-button type="default">默认按钮</van-button>
     <van-button type="primary">主要按钮</van-button>
     <van-button type="info">信息按钮</van-button>
@@ -11,6 +14,7 @@
 </template>
 
 <script>
+import '@/styles/base.less'
 export default {
   name: 'App'
 }
--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -4,3 +4,4 @@ import {
 } from 'vant' // 在引入Vant相关的组件模块
 
 Vue.use(Button) // 全局注册相关组件模块
+// .use(Icon) // （链式调用）注册Icon图标字体组件模块
