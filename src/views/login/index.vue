2e0a9a9947cb58375b380e2241a2d6b4a35bce7e
xuhaofeng <xuhaofeng@163.com>
2020-01-08 16:57:03 +0800

    用户登录注册-实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,22 +4,51 @@
     <van-nav-bar title="登录" />
     <!--引入输入框-->
     <div class="login-field">
-      <van-field placeholder="请输入用户名"  left-icon="user-o">
+      <van-field
+      placeholder="请输入用户名"
+      left-icon="user-o"
+      v-model="user.mobile"
+      >
       </van-field>
-      <van-field placeholder="请输入验证码">
+      <van-field placeholder="请输入验证码" v-model="user.code">
          <van-icon slot="left-icon" class-prefix="icont" name="mima" />
       </van-field>
 
     </div>
     <div class="loginBtn">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="onLogin">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request-axios'
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      // 1,获取数据
+      const user = this.user
+      console.log(user)
+
+      // 2，请求参数
+      let result = await request({
+        url: '/app/v1_0/authorizations',
+        method: 'post', // 请求方式
+        data: user
+      })
+      // 3，返回数据
+      console.log(result)
+    }
+  }
 }
 </script>
 
