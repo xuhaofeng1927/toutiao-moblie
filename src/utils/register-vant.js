faa14887811199ad07734f190a53194b727b83a2
xuhaofeng <xuhaofeng@163.com>
2020-01-08 16:28:48 +0800

    用户登录注册-布局样式

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,7 +1,12 @@
 import Vue from 'vue' // 先引入Vue实例对象
 import {
-  Button, Icon
+  Button,
+  Icon,
+  NavBar,
+  Field
 } from 'vant' // 在引入Vant相关的组件模块
 
 Vue.use(Button) // 全局注册相关组件模块
   .use(Icon) // （链式调用）注册Icon图标
+  .use(NavBar) // 引入NavBar导航栏
+  .use(Field) // 引入button按钮
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="login">
-    登陆页面
+    <!--引入导航 -->
+    <van-nav-bar title="登录" />
+    <!--引入输入框-->
+    <div class="login-field">
+      <van-field placeholder="请输入用户名"  left-icon="user-o">
+      </van-field>
+      <van-field placeholder="请输入验证码" left-icon="eye-o">
+         <van-icon class-prefix="icont" name="mima" />
+      </van-field>
+
+    </div>
+    <div class="loginBtn">
+      <van-button type="info">登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +23,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+  .login-field {
+    margin-top:20px;
+    .van-cell {
+      height: 50px;
+      padding: 10px 20px;
+      align-items: center;
+      .login-btn {
+      width: 100%;
+    }
+    }
 
+  }
+  .loginBtn {
+    margin-top:20px;
+   padding: 27px 16px;
+    .van-button {
+      width: 100%;
+      background-color: #6db4fb
+    }
+  }
 </style>
