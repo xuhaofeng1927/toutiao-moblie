0a46937367c85ba6bcb41ed23fba8060e22a1f36
xuhaofeng <xuhaofeng@163.com>
2020-01-08 17:38:06 +0800

    用户登录注册-登录中提示

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -3,10 +3,12 @@ import {
   Button,
   Icon,
   NavBar,
-  Field
+  Field,
+  Toast
 } from 'vant' // 在引入Vant相关的组件模块
 
 Vue.use(Button) // 全局注册相关组件模块
   .use(Icon) // （链式调用）注册Icon图标
   .use(NavBar) // 引入NavBar导航栏
   .use(Field) // 引入button按钮
+  .use(Toast) // 引入轻提示
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,16 +4,10 @@
     <van-nav-bar title="登录" />
     <!--引入输入框-->
     <div class="login-field">
-      <van-field
-      placeholder="请输入用户名"
-      left-icon="user-o"
-      v-model="user.mobile"
-      >
-      </van-field>
+      <van-field placeholder="请输入用户名" left-icon="user-o" v-model="user.mobile"></van-field>
       <van-field placeholder="请输入验证码" v-model="user.code">
-         <van-icon slot="left-icon" class-prefix="icont" name="mima" />
+        <van-icon slot="left-icon" class-prefix="icont" name="mima" />
       </van-field>
-
     </div>
     <div class="loginBtn">
       <van-button type="info" @click="onLogin">登录</van-button>
@@ -35,42 +29,52 @@ export default {
   },
   methods: {
     async onLogin () {
+      // 使用轻提示
+      this.$toast.loading({
+        duration: 0, // 持续时间，0表示持续展示不停止
+        forbidClick: true, // 是否禁止背景点击
+        message: '登录中...' // 提示消息
+      })
       // 1,获取数据
       const user = this.user
       console.log(user)
-
-      // 2，请求参数
-      let result = await request({
-        url: '/app/v1_0/authorizations',
-        method: 'post', // 请求方式
-        data: user
-      })
-      // 3，返回数据
-      console.log(result)
+      try {
+        // 2，请求参数
+        let result = await request({
+          url: '/app/v1_0/authorizations',
+          method: 'post', // 请求方式
+          data: user
+        })
+        // 3，返回数据
+        console.log(result)
+        this.$toast.success('登录成功')
+      } catch (error) {
+        console.log('失败', error)
+        this.$toast.fail('用户名或密码错误')
+      }
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .login-field {
-    margin-top:20px;
-    .van-cell {
-      height: 50px;
-      padding: 10px 20px;
-      align-items: center;
-      .login-btn {
+.login-field {
+  margin-top: 20px;
+  .van-cell {
+    height: 50px;
+    padding: 10px 20px;
+    align-items: center;
+    .login-btn {
       width: 100%;
     }
-    }
-
   }
-  .loginBtn {
-    margin-top:20px;
-   padding: 27px 16px;
-    .van-button {
-      width: 100%;
-      background-color: #6db4fb
-    }
+}
+.loginBtn {
+  margin-top: 20px;
+  padding: 27px 16px;
+  .van-button {
+    width: 100%;
+    background-color: #6db4fb;
   }
+}
 </style>
