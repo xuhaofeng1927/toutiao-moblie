42dc40d090c3414fd8199a6d8aeb48c9426b654e
xuhaofeng <xuhaofeng@163.com>
2020-01-09 16:31:05 +0800

    用户登录注册-手动触发验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,20 +4,19 @@
     <van-nav-bar title="登录" />
     <!--引入输入框-->
     <div class="login-field">
-       <!--
+      <!--
       表单验证
       1、使用 ValidationObserver 组件把需要验证的整个表单包起来
       2、使用 ValidationProvider 组件把具体的表单元素包起来，例如 input
          name   配置字段的提示名称
          rules  配置校验规则
          v-slot="{ errors }" 获取校验失败的错误提示消息
-     -->
-      <ValidationObserver>
-        <ValidationProvider name="手机号" rules="required" v-slot="{ errors }">
+      -->
+      <ValidationObserver ref="myform">
+        <ValidationProvider name="手机号" rules="required">
           <van-field placeholder="请输入用户名/手机号/邮箱" left-icon="user-o" v-model="user.mobile"></van-field>
-          <span>{{ errors[0] }}</span>
         </ValidationProvider>
-        <ValidationProvider>
+        <ValidationProvider name="验证码" rules="required">
           <van-field placeholder="请输入验证码" v-model="user.code">
             <van-icon slot="left-icon" class-prefix="icont" name="mima" />
             <van-button
@@ -52,7 +51,7 @@ export default {
   data () {
     return {
       user: {
-        mobile: '',
+        mobile: '13911111111',
         code: ''
       },
       isCountDownShow: true,
@@ -62,42 +61,62 @@ export default {
   methods: {
     // 用户登录
     async onLogin () {
-      // 使用轻提示
-      this.$toast.loading({
-        duration: 0, // 持续时间，0表示持续展示不停止
-        forbidClick: true, // 是否禁止背景点击
-        message: '登录中...' // 提示消息
-      })
-      // 1,获取数据
-      const user = this.user
-      console.log(user)
-      try {
-        // 2，请求参数
-        let result = await getUsersLogin(user)
-        // 3，返回数据
-        console.log('成功', result)
-        this.$toast.success('登录成功')
-      } catch (error) {
-        console.log('失败', error)
-        this.$toast.fail('用户名或密码错误')
+      // 手动触发校验
+      let result = await this.$refs.myform.validate() // 异步转为同步(result为boolen值)
+      if (!result) {
+        // 取反不存在时执行返回
+        const errors = this.$refs.myform.errors // 获取错误返回对象
+        for (let key in errors) {
+          const item = errors[key]
+          if (item[0]) {
+            this.$toast(item[0]) // 当符合条件时返回错误提示信息
+            return // 符合条件时返回
+          }
+        }
+      } else {
+        // 存在时执行以下代码
+
+        // 使用轻提示
+        this.$toast.loading({
+          duration: 0, // 持续时间，0表示持续展示不停止
+          forbidClick: true, // 是否禁止背景点击
+          message: '登录中...' // 提示消息
+        })
+        try {
+        // 1,获取数据
+          const user = this.user
+          console.log(user)
+          // 2，请求参数
+          let result = await getUsersLogin(user)
+          // 3，返回数据
+          console.log('成功', result)
+          this.$toast.success('登录成功')
+        } catch (error) {
+          console.log('失败', error)
+          this.$toast.fail('用户名或密码错误')
+        }
       }
     },
     // 获取验证码
     async getverity () {
       // 验证手机号是否为空
       if (this.user.mobile) {
-        // 隐藏标签
-        this.isCountDownShow = false
-
+        // 获取手机号
         const { mobile } = this.user
         // 验证手机号是否存在
+        // 隐藏标签
+        this.isCountDownShow = false
 
         // 请求数据
         try {
           let result = await getverity(mobile)
           console.log('成功', result)
         } catch (error) {
-          this.$toast.fail('请勿频繁操作')
+          if (error.response.status === 429) {
+            this.$toast('请勿频繁发送')
+          } else {
+            this.$toast.fail('发送失败')
+          }
         }
       } else {
         this.$toast.fail('请输入手机号')
