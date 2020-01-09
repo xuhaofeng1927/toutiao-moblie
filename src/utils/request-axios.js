3a80fcfaf77e9e02193b3245409a920ebb12485b
xuhaofeng <xuhaofeng@163.com>
2020-01-09 22:15:13 +0800

    用户登录注册-发送 token（请求拦截器）

--- src/utils/request-axios.js
+++ src/utils/request-axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios' // 引入 axios
 import jsonBig from 'json-bigint' // 引入jsonBig
+import store from '@/store'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 配置基础地址
@@ -16,4 +17,24 @@ request.defaults.transformResponse = [function (data) {
     return {} // 不存在返回一个空数组
   }
 }]
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+  // 成功时执行代码
+  // config 请求配置对象，我们可以通过修改 config 来实现统一请求数据处理
+  const { user } = store.state
+
+  // 统一添加 token
+  if (user) {
+    // config.headers 获取操作请求头对象
+    // Authorization 是后端要求的字段名称
+    // 数据值后端要求提供：Bearer token数据
+    //    注意：Bearer 后面有个空格
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
 export default request //  导出新的axios
