4c76f2005fab0062952f624d715c2d12ee3b98ef
xuhaofeng <xuhaofeng@163.com>
2020-01-13 18:07:36 +0800

    首页-频道编辑-编辑频道 -处理编辑状态

--- src/views/home/A-homePage/components/channels-list.vue
+++ src/views/home/A-homePage/components/channels-list.vue
@@ -3,11 +3,13 @@
 <template>
   <div class="channel-edit">
     <van-cell title="我的频道" :border="false">
-      <van-button size="mini" round type="danger" plain>编辑</van-button>
+      <van-button size="mini" round type="danger" plain @click="openClose">{{isEditShow?'完成':'编辑'}}</van-button>
     </van-cell>
 
     <van-grid :gutter="10">
-      <van-grid-item v-for="(item,index) in Channelslist" :key="index" :text="item.name" />
+      <van-grid-item v-for="(item,index) in Channelslist" :key="index" :text="item.name">
+        <van-icon v-show="isEditShow && index !== 0" slot="icon" name="close" />
+      </van-grid-item>
     </van-grid>
 
     <van-cell title="推荐频道" :border="false" />
@@ -25,34 +27,26 @@
 <script>
 import { getAllChannels } from '@/api/channels'
 export default {
-  props: {
-    Channelslist: {
-      type: Array,
-      required: true
-    }
-  }, // 接收父组件传过来的属性值
+  props: ['Channelslist'], // 接收父组件传过来的属性值
   data () {
     return {
-      allChannels: [] // 所有频道
+      allChannels: [], // 所有频道
+      isEditShow: false // 控制删除按钮的开关
     }
   },
   computed: {
     remanentChannels () {
-      // console.log(1)
       const { Channelslist, allChannels } = this // 获取用户列表和所有列表
-      // console.log(Channelslist)
       const channels = [] // 定义一个新的数组用来接收剩余频道
       // 推荐频道 = 所有频道 - 用户列表
       // remanentChannels = allChannels - Channelslist
       if (allChannels) {
         // 遍历所有频道
         allChannels.forEach(item => {
-          console.log(item)
           if (!Channelslist.find(i => i.id === item.id)) {
             channels.push(item) // 遍历每一项不等与用户列表数据的值
           }
         })
-        // console.log(channels)
         return channels // 返回获取到的剩余频道列表值
       } else {
         this.$toast('无法获取频道信息')
@@ -71,6 +65,10 @@ export default {
     // 推荐添加到我的频道
     onChannelAdd (params) {
       this.Channelslist.push(params) // 频道中添加要添加的频道
+      // 不需要删除，我的频道改变，计算属性 recommendChannels 重新调用求值
+    },
+    openClose () {
+      this.isEditShow = !this.isEditShow
     }
   },
   created () {
@@ -82,5 +80,14 @@ export default {
 <style lang="less" scoped>
 .channel-edit {
   padding: 40px 18px 0;
+  /deep/ .van-grid-item__icon-wrapper {
+  position: absolute;
+  top: -22px;
+  right: -7px;
+  .van-icon-close {
+    font-size: 14px;
+    color: red
+  }
+}
 }
 </style>
