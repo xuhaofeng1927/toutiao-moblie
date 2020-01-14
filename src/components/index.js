fc9ef801e9a6b4d8c0df483f533f0f9549b3eded
xuhaofeng <xuhaofeng@163.com>
2020-01-14 21:34:38 +0800

    首页-文章搜索-处理页面显示状态

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import ArticleList from './article-list'
 import channelsList from './channels-list'
+import searchResult from './search-result'
 export default {
   install (Vue) {
     Vue.component('Article-list', ArticleList) // 全局封装组件
     Vue.component('Channels-list', channelsList) // 全局封装组件
+    Vue.component('Search-result', searchResult) // 全局封装组件
   }
 }
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="search-container">
+
     <!-- 搜索栏 -->
     <form action="/" class="search-form">
       <van-search
@@ -12,9 +13,12 @@
       />
     </form>
     <!-- /搜索栏 -->
+      <Search-result v-if="isResultShow"></Search-result>
+     <!-- 搜索结果 -->
+    <!-- /搜索结果 -->
 
     <!-- 联想建议 -->
-    <van-cell-group>
+    <van-cell-group v-else-if="searchContent">
       <van-cell icon="search" title="单元格" />
       <van-cell icon="search" title="单元格" />
       <van-cell icon="search" title="单元格" />
@@ -25,13 +29,14 @@
     <!-- /联想建议 -->
 
     <!-- 历史记录 -->
-    <van-cell title="历史记录">
-      <van-icon name="delete" />
-      <span>全部删除</span>
-      &nbsp;&nbsp;
-      <span>完成</span>
-    </van-cell>
-    <van-cell-group>
+    <van-cell-group v-else>
+      <van-cell title="历史记录">
+        <van-icon name="delete" />
+        <span>全部删除</span>
+        &nbsp;&nbsp;
+        <span>完成</span>
+      </van-cell>
+
       <van-cell title="单元格">
         <van-icon name="close"></van-icon>
       </van-cell>
@@ -50,62 +55,26 @@
     </van-cell-group>
     <!-- /历史记录 -->
 
-    <!-- 搜索结果 -->
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      @load="onLoad"
-    >
-      <van-cell
-        v-for="item in list"
-        :key="item"
-        :title="item"
-      />
-    </van-list>
-    <!-- /搜索结果 -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchPage',
-  components: {},
   props: {},
   data () {
     return {
       searchContent: '', // 搜索内容
-      list: [],
-      loading: false,
-      finished: false
+      // 控制搜索结果的显示状态
+      isResultShow: false
     }
   },
-  computed: {},
-  watch: {},
-  created () {},
-  mounted () {},
   methods: {
     onSearch () {
       console.log('onSearch')
     },
     onCancel () {
       console.log('onCancel')
-    },
-
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
     }
   }
 }
