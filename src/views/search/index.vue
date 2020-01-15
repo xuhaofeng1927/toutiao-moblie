66fc1b32ad44511bed9be664d487607687ae297f
xuhaofeng <xuhaofeng@163.com>
2020-01-15 17:02:31 +0800

    首页-文章搜索-删除历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -8,7 +8,7 @@
         show-action
         background="#3296fa"
         @search="onSearch(searchContent)"
-        @cancel="onCancel"
+        @cancel="onCancel(searchContent)"
         @input="onSearchInput"
       />
     </form>
@@ -19,7 +19,13 @@
 
     <!-- 联想建议 -->
     <van-cell-group v-else-if="searchContent">
-      <van-cell icon="search" :title="item" v-for="(item,index) in suggestions" @click="onSearch(item)" :key="index">
+      <van-cell
+        icon="search"
+        :title="item"
+        v-for="(item,index) in suggestions"
+        @click="onSearch(item)"
+        :key="index"
+      >
         <!-- 我们要把 item 处理成带有高亮的字符串 -->
         <!-- 过滤器：专门用于文本格式化，但是它只能用在 {{}} 和 v-bind 中 -->
         <div slot="title" v-html="lightText(item)"></div>
@@ -30,14 +36,14 @@
     <!-- 历史记录 -->
     <van-cell-group v-else>
       <van-cell title="历史记录">
-        <van-icon name="delete" />
-        <span>全部删除</span>
+        <van-icon name="delete" v-show="!isDeleteShow" @click="isDeleteShow=!isDeleteShow" />
+        <span v-show="isDeleteShow" @click="searchHistories = []">全部删除</span>
         &nbsp;&nbsp;
-        <span>完成</span>
+        <span v-show="isDeleteShow" @click="isDeleteShow=!isDeleteShow">完成</span>
       </van-cell>
 
-      <van-cell :title="item" v-for="(item,index) in searchHistories" :key="index">
-        <van-icon name="close"></van-icon>
+      <van-cell :title="item" v-for="(item,index) in searchHistories" :key="index" @click="onHistoryClick(item,index)">
+        <van-icon name="close" v-show="isDeleteShow"></van-icon>
       </van-cell>
     </van-cell-group>
     <!-- /历史记录 -->
@@ -56,11 +62,12 @@ export default {
       // 控制搜索结果的显示状态
       isResultShow: false,
       suggestions: [], // 联想记忆数据
-      searchHistories: [] // 搜索历史记录数据
-
+      searchHistories: [], // 搜索历史记录数据
+      isDeleteShow: false // 控制历史记录删除条件
     }
   },
   methods: {
+    // 搜索时执行
     onSearch (value) {
       // value: 输入框当前值
       // 更新搜索文本框的数据(三种状态输入关键字，联想建议关键字，历史记录关键字)
@@ -76,9 +83,21 @@ export default {
       // 搜索结果组件显示状态
       this.isResultShow = true
     },
-    onCancel () {
-      console.log('onCancel')
+    onCancel (value) {
+      if (value) {
+        console.log('onCancel')
+      }
     },
+    onHistoryClick (item, index) {
+      // 如果是删除状态，则执行删除操作
+      if (this.isDeleteShow) {
+        this.searchHistories.splice(index, 1)
+      } else {
+        // 否则执行搜索操作
+        this.onSearch(item)
+      }
+    },
+
     // debounce 函数
     // 参数1：函数
     // 参数2：防抖时间
@@ -98,7 +117,10 @@ export default {
     // 替换高亮字体方法（正则表达）
     lightText (str) {
       const reg = new RegExp(this.searchContent, 'ig')
-      return str.replace(reg, `<span style="color: deeppink">${this.searchContent}</span>`)
+      return str.replace(
+        reg,
+        `<span style="color: deeppink">${this.searchContent}</span>`
+      )
     }
   }
 }
