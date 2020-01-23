dfa53e8d1d01710d377beb251d046eb52883cf0f
xuhaofeng <xuhaofeng@163.com>
2020-01-23 19:00:45 +0800

    评论回复-展示评论回复列表

--- src/components/reply-comment.vue
+++ src/components/reply-comment.vue
@@ -4,10 +4,15 @@
       <van-icon name="cross" slot="left" />
     </van-nav-bar>
     <Comment-detail :comment="comment"></Comment-detail>
+    <van-cell title="所有评论" />
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <Comment-detail :comment="comment" v-for="(comment,index) in list" :key="index"></Comment-detail>
+    </van-list>
   </div>
 </template>
 
 <script>
+import { getComments } from '@/api/comment'
 export default {
   name: 'replyComment',
   props: {
@@ -16,7 +21,38 @@ export default {
       required: true
     }
   },
-  methods: {}
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      offset: null,
+      limit: 20
+    }
+  },
+  methods: {
+    // 展示回复列表
+    async onLoad () {
+      // 1,请求获取列表数据
+      const { data } = await getComments({
+        type: 'c',
+        source: this.comment.com_id.toString(),
+        offset: this.offset,
+        limit: this.limit
+      })
+      const { results } = data.data
+      // 2，将数据添加到列表
+      this.list.push(...results)
+      // 3，关闭loading
+      this.loading = true
+      // 4，判断是否还有数据
+      if (results.length) {
+        this.offset = data.data.last_id
+      } else {
+        this.finished = true
+      }
+    }
+  }
 }
 </script>
 
