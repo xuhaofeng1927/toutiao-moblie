4ec18566571f9213433848cd1326ccbef1c949a6
xuhaofeng <xuhaofeng@163.com>
2020-02-02 21:58:33 +0800

    用户信息-修改信息-修改头像（组件调用）

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -5,33 +5,43 @@
     <!-- /导航信息 -->
     <van-cell-group>
       <van-cell title="头像" @click="onSelectFile">
-        <van-image
-          round
-          width="30"
-          height="30"
-          fit="cover"
-          :src="user.photo"
-        />
-        <input type="file" hidden ref="file" @change="onFileChange">
+        <van-image round width="30" height="30" fit="cover" :src="user.photo" />
+        <input type="file" hidden ref="file" @change="onFileChange" />
       </van-cell>
       <van-cell title="昵称" :value="user.name" is-link />
       <van-cell title="介绍" value="内容" is-link />
       <van-cell title="性别" :value="user.gender?'女':'男'" is-link />
       <van-cell title="生日" :value="user.birthday" is-link />
     </van-cell-group>
+    <!-- 头像预览 -->
+    <van-image-preview v-model="isHeadShow" :images="images" @close="$refs.file.value = ''">
+      <van-nav-bar
+        slot="cover"
+        left-text="取消"
+        right-text="确定"
+        @click-left="isHeadShow = false"
+        @click-right="onUpdateAvatar"
+      />
+    </van-image-preview>
+    <!-- /头像预览 -->
   </div>
 </template>
 
 <script>
 import { getPersonUserxInfo, updateUserPhoto } from '@/api/user'
-import { ImagePreview } from 'vant'
+// import { ImagePreview } from 'vant'
 export default {
   data () {
     return {
-      user: {} // 接收用户个人信息
+      user: {}, // 接收用户个人信息
+      isHeadShow: false,
+      images: [] // 预览图片列表
     }
   },
   computed: {
+    // 这里的目的主要是为了访问方便
+    // 每次访问 ref 成员都需要：this.$refs['file']
+    // 因为多次访问到了该成员，所以我可以使用计算属性封装简化对成员的访问
     file () {
       return this.$refs['file']
     }
@@ -55,36 +65,32 @@ export default {
       // 通过 file-input 获取文件对象
       const fileObj = this.file.files[0] // files代表为文件dom对象的一个属性
       const fileData = window.URL.createObjectURL(fileObj)
-      ImagePreview({
-        images: [fileData],
-        // 预览关闭后的
-        onClose: this.savePhoto()
-      })
+      // 获取图片文件
+      this.images = [fileData]
+      // 显示预览图片
+      this.isHeadShow = true
     },
-    async savePhoto () {
-      await this.$dialog.confirm({
-        title: '头像选择',
-        message: '确认使用该图片作为头像吗？'
-      })
+    // 预览图片确定时请求数据
+    async onUpdateAvatar () {
+      this.isHeadShow = false
       this.$toast.loading({
         duration: 0, // 持续展示 toast
-        message: '保存中...',
+        message: '头像上传中...',
         forbidClick: true // 是否禁止背景点击
       })
       try {
-        const fileObj = this.file.files[0] // files代表为文件dom对象的一个属性
         // 构造包含文件的表单对象
         const fd = new FormData()
-        fd.append('photo', fileObj)
-        // fd.append('photo', fileObj)
-        await updateUserPhoto(fd)
+        // this.file.files[0] files代表为文件dom对象的一个属性
+        fd.append('photo', this.file.files[0])
+        const { data } = await updateUserPhoto(fd)
         // 更新视图
-        this.user.photo = URL.createObjectURL(fileObj)
+        this.user.photo = data.data.photo
         // 提示成功
-        this.$toast.success('保存成功')
+        this.$toast.success('上传成功')
       } catch (error) {
         console.log(error)
-        this.$toast.fail('保存失败')
+        this.$toast.fail('上传失败')
       }
     }
   },
@@ -94,8 +100,17 @@ export default {
 }
 </script>
 
-<style scoped lan="less">
+<style scoped lang="less">
 .van-nav-bar__text {
   color: white;
+}
+/deep/ .van-image-preview__cover {
+  top: unset;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  .van-nav-bar {
+    background: #000;
+  }
 }
 </style>
