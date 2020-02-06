e34157cd8f34b682ddcf7d61b2c641c2a0a5165e
xuhaofeng <xuhaofeng@163.com>
2020-02-06 21:20:39 +0800

    我的收藏历史-页面布局

--- src/views/user-articles/index.vue
+++ src/views/user-articles/index.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="user-articles">用户文章</div>
+  <div class="user-articles">
+    <!-- 导航栏 -->
+    <van-nav-bar title="我的收藏/历史/作品" left-arrow @click-left="$router.back()" />
+    <!-- /导航栏 -->
+
+    <van-tabs v-model="active" swipeable>
+      <van-tab title="我的收藏">内容 1</van-tab>
+      <van-tab title="我的历史">内容 2</van-tab>
+      <van-tab title="我的作品">
+        <my-production/>
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
+import myProduction from './components/my-production'
 export default {
   name: 'UserArticles',
   data () {
-    return {}
+    return {
+      active: 2
+    }
+  },
+  components: {
+    myProduction
   },
   methods: {}
 }
